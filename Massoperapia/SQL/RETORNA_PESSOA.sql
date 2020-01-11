00d67265ce95c4fd6242dc7a4eb7ba3def1c1497
Gabriel Motta <gabrielmottadev@gmail.com>
2020-01-11 17:35:17 -0200

    PROCEDURES

---
+++ Massoperapia/SQL/RETORNA_PESSOA.sql
@@ -0,0 +1,18 @@
+--------------------------------------------------------
+--  Arquivo criado - Sábado-Janeiro-11-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Procedure RETORNA_PESSOA
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE NONEDITIONABLE PROCEDURE "T12SPS4"."RETORNA_PESSOA" 
+
+
+ AS
+CID NUMBER;
+BEGIN
+  SELECT ID INTO CID  FROM T12SPS4.PESSOA;
+END RETORNA_PESSOA;
+
+/
